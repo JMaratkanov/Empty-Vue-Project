85872be88131624d7e473639d1f5fc6c23a088bb
Johann M <j.maratkanov@outlook.de>
2022-12-19 10:50:58 +0100

    Kleines Projekt

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import store from "@/store";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+
 
 const app = createApp(App);
 
-app.use(router);
+app.use(VueAxios, axios);
+app.use(store);
 
 app.mount("#app");
